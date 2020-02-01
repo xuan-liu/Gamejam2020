a0eb0931808ac959246c9e0e667cbb910cae7767
xuan-liu <lxuan95@163.com>
2020-02-01 14:28:46 -0800

    add fox
    add fox character

---
+++ Assets/KeepScore.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepScore : MonoBehaviour
+{
+    public static int Score= 0; //Initialize the score to zero
+
+    private void OnGUI()
+    {
+        // Create style for a button
+        GUIStyle myTextStyle = new GUIStyle(GUI.skin.button);
+        myTextStyle.fontSize = 50;
+
+        //// Load and set Font
+        //Font myFont = (Font)Resources.Load("Fonts/comic", typeof(Font));
+        //myTextStyle.font = myFont;
+        //// Set color for selected and unselected buttons
+        //myTextStyle.normal.textColor = Color.red;
+        //myTextStyle.hover.textColor = Color.red;
+
+
+        //OnGUI is called for rendering and handling GUI events.
+        GUI.Box(new Rect(10, 10, 250, 150), "Score: " + Score.ToString(), myTextStyle);
+        //Creates a rectangle in the top left of the screen to display score
+    }
+}
---
+++ Assets/PlayerDash.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDash : MonoBehaviour
+{
+    public Vector3 moveDirection;
+
+    public const float maxDashTime = 1.0f;
+    public float dashDistance = 10;
+    public float dashStoppingSpeed = 0.1f;
+    float currentDashTime = maxDashTime;
+    float dashSpeed = 6;
+    CharacterController controller;
+
+
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z)) //Right mouse button
+        {
+            currentDashTime = 0;
+        }
+        if (currentDashTime < maxDashTime)
+        {
+            moveDirection = transform.forward * dashDistance;
+            currentDashTime += dashStoppingSpeed;
+        }
+        else
+        {
+            moveDirection = Vector3.zero;
+        }
+        controller.Move(moveDirection * Time.deltaTime * dashSpeed);
+    }
+}
