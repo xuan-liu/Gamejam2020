428ec22a84732bc2c6b3044c608f1f3682df98aa
Yihui Liu <Yihui0602@gmail.com>
2020-02-01 18:00:00 -0800

    Splash Scene

---
+++ Assets/LevelLoad.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelLoad : MonoBehaviour
+{
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+
+
+    }
+}
