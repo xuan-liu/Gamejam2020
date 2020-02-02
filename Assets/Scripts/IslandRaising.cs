70e2c313ffd1441822786de0f7be098cbc1d6652
Ke Jing <kjing1@uci.edu>
2020-02-02 05:35:38 -0800

    level1

---
+++ Assets/Scripts/IslandRaising.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IslandRaising : MonoBehaviour
+{
+    public Vector3 start;
+    public Vector3 end;
+
+    bool isMoving = false;
+
+    float speed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = start;
+    }
+
+    private void OnEnable()
+    {
+        isMoving = true;
+    }
+    private void OnDisable()
+    {
+        isMoving = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMoving)
+        {
+            transform.position = Vector3.Lerp(transform.position, end, speed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, end) < 0.1f)
+                isMoving = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+    public Vector3 start;
+    public Vector3 end;
+
+    public float speed = 1;
+    float dis;
+    enum State { Start, End};
+    State state;
+
+    public const float closeThreshold = 0.1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dis = Vector3.Distance(end, start);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = Vector3.Lerp(start, end, Mathf.PingPong(Time.time, 1.0f));
+
+        switch (state)
+        {
+            case State.Start:
+                transform.position = Vector3.Lerp(transform.position, end, speed * Time.deltaTime);
+                if(Vector3.Distance(transform.position, end) < closeThreshold)                
+                    state = State.End;
+                break;
+            case State.End:
+                transform.position = Vector3.Lerp(transform.position, start, speed * Time.deltaTime);
+                if(Vector3.Distance(transform.position, start) < closeThreshold)                
+                    state = State.Start;
+                break;
+
+        }
+    }
+}
